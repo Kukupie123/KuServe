9a681236e09b47a45e26f09590bdf426fcaea989
kukupie123 <kukuborom@gmail.com>
2022-04-23 22:07:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# youtube_playlist_combiner
+
+Get songs from multiple Playlists
+Play them in order or shuffled
+Save them in a new playlist
---
+++ android/app/src/main/kotlin/com/kukukode/youtube_playlist_combiner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kukukode.youtube_playlist_combiner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
