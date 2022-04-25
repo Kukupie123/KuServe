e7660f10812f7cd103612bc136f3e87b5e0c7493
kukupie123 <kukuborom@gmail.com>
2022-04-26 00:58:55 +0530

    minor fix

--- lib/pages/SongPlayer/components/SongsPlayerWidget.dart
+++ lib/pages/SongPlayer/components/SongsPlayerWidget.dart
@@ -78,75 +78,64 @@ class _SongsPlayerWidgetState extends State<SongsPlayerWidget> {
     //Update the variable of provider. This is going to be used to show list of songs to user
 
     return Scaffold(
-      body: YoutubeValueBuilder(
-        controller: _yts,
-        builder: (BuildContext context, value) {
-          if (value.isReady && !value.hasPlayed) {
-            _yts.play();
-            if (kDebugMode) {
-              print("Video is playing");
-            }
-          }
-          return SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Stack(
-              children: [
-                //Background Image
-                ImageFiltered(
-                  imageFilter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-                  child: CachedNetworkImage(
-                    imageUrl:
-                        "https://img3.goodfon.com/wallpaper/nbig/4/12/3d-fioletovyy-krugi-svechenie.jpg",
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                ),
-
-                //Sub container in middle
-                // ignore: prefer_const_constructors
-                Align(
-                  alignment: AlignmentDirectional.center,
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                        sigmaX: 10, sigmaY: 8, tileMode: TileMode.clamp),
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 1500),
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      height: MediaQuery.of(context).size.height * 0.85,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.elliptical(20, 20),
-                        ),
-                        gradient: LinearGradient(
-                          colors: [
-                            colors[rand.nextInt(colors.length - 1)],
-                            colors[rand.nextInt(colors.length - 1)],
-                            colors[rand.nextInt(colors.length - 1)],
-                            colors[rand.nextInt(colors.length - 1)],
-                          ],
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            width: 650,
-                            height: 650,
-                            child: YoutubePlayerIFrame(
-                              controller: _yts,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+        body: SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          //Background Image
+          ImageFiltered(
+            imageFilter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+            child: CachedNetworkImage(
+              imageUrl:
+                  "https://img3.goodfon.com/wallpaper/nbig/4/12/3d-fioletovyy-krugi-svechenie.jpg",
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
             ),
-          );
-        },
+          ),
+
+          //Sub container in middle
+          // ignore: prefer_const_constructors
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                  sigmaX: 10, sigmaY: 8, tileMode: TileMode.clamp),
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 1500),
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.85,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.elliptical(20, 20),
+                  ),
+                  gradient: LinearGradient(
+                    colors: [
+                      colors[rand.nextInt(colors.length - 1)],
+                      colors[rand.nextInt(colors.length - 1)],
+                      colors[rand.nextInt(colors.length - 1)],
+                      colors[rand.nextInt(colors.length - 1)],
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 650,
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      child: YoutubePlayerIFrame(
+                        controller: _yts,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
